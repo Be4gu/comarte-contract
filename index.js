c49f484b81d2a3e3e9eff8d2e62e8d6b01771da4
Be4gu <ioncristicristea@gmail.com>
2024-09-23 11:51:44 +0200

    Tercer commit

--- index.js
+++ index.js
@@ -4,27 +4,45 @@ const app = express()
 const db = [
   {
     id: '123',
-    contract: '001'
+    contract: '001',
+    plan: 'Silver',
+    apis: ['consolidatedFinancialConnections'],
+    limits: '5000'
   },
   {
     id: '234',
-    contract: '002'
+    contract: '002',
+    plan: 'Platinum',
+    apis: [],
+    limits: '100000'
   },
   {
-    id: '345',
-    contract: '003'
+    clientid: '345',
+    contract: '003',
+    plan: 'Platinum',
+    apis: [],
+    limits: '100000'
   },
   {
     id: '456',
-    contract: '004'
+    contract: '004',
+    plan: 'Bronze',
+    apis: ['Tenders'],
+    limits: '1000'
   },
   {
     id: '567',
-    contract: '005'
+    contract: '005',
+    plan: 'Bronze',
+    apis: ['Tenders', 'Investigated'],
+    limits: '1000'
   },
   {
     id: '678',
-    contract: '006'
+    contract: '006',
+    plan: 'Platino',
+    apis: [],
+    limits: '100000'
   }
 ]
 app.get('/msg/:contract/:id', (req, res, next) => {
@@ -39,7 +57,8 @@ app.get('/msg/:contract/:id', (req, res, next) => {
   const user = db.find((elem) => elem.contract === req.params.contract)
   if (user.contract !== req.params.contract || user.id !== req.params.id) {
     res.status(404)
-    return res.json({ message: 'Credentials not match' })
+    return res.json({ message: 'Credentials not matchs' })
   }
-  res.send('Todo encontrado')
+  res.status(200)
+  res.json(user)
 })
